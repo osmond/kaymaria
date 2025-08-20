fdb4bf16aa554c73bbb96ba8cf65c52571ec395f
Jon Osmond <jonathan.osmond@gmail.com>
2025-08-19 22:21:28 -0500

    test: use userEvent for ChipSelect

--- components/ChipSelect.test.tsx
+++ components/ChipSelect.test.tsx
@@ -1,8 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { ChipSelect } from './ChipSelect';
@@ -19,7 +18,7 @@ describe('ChipSelect', () => {
     await user.keyboard('{ArrowRight}');
     const second = getByLabelText('B');
     expect(document.activeElement).toBe(second);
-    fireEvent.keyDown(second, { key: ' ' });
+    await user.keyboard('{Space}');
     expect(onChange).toHaveBeenCalledWith('B');
   });
 
